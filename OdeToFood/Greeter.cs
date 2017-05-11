048147c6aa32aba939e2f13b6e659a86800cf8ec
stanleyalv3s <comercial@rioinforma.com.br>
2017-05-11 15:54:41 -0300

    Add project files.

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+    
+  
+    public interface IGreeter
+    {
+        string GetGreeting();
+
+    }
+
+                                                                                   
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuraton)
+        {
+            _greeting = configuraton["Greerting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+
+    }
+    
+}
